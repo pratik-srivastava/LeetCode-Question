96f1f15e2a3be10d204bfc3a7da7fc2b91c097a4
Pratik Kumar Srivastava <86921160+pratik-srivastava@users.noreply.github.com>
2022-02-24 10:12:57 +0530

    Add files via upload

---
+++ PlusOne(Q66).py
@@ -0,0 +1,11 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        s=''.join(str(i) for i in digits)
+        q=int(s)+1
+        st=str(q)
+        return st
+    
+    
+    
+    
+    
