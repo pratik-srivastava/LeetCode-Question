821f54c6b43c10ae819af5f4616ce229ea74dec0
Pratik Kumar Srivastava <86921160+pratik-srivastava@users.noreply.github.com>
2022-04-15 22:45:11 +0530

    Time: 2738 ms (11.22%), Space: 14 MB (12.66%) - LeetHub

---
+++ 69-sqrtx/69-sqrtx.py
@@ -0,0 +1,9 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        for i in range(x+1):
+            if i*i == x:
+                return i
+            elif i*i>x:
+                return i-1
+            
+        
