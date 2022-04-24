70efac14a5ab7a70157e13545b85e054056a2240
Pratik Kumar Srivastava <86921160+pratik-srivastava@users.noreply.github.com>
2022-04-24 17:48:55 +0530

    Time: 32 ms (81.66%), Space: 13.9 MB (67.29%) - LeetHub

--- 374-guess-number-higher-or-lower/374-guess-number-higher-or-lower.py
+++ 374-guess-number-higher-or-lower/374-guess-number-higher-or-lower.py
@@ -20,16 +20,3 @@ class Solution:
                 return m
         
         
-#         low = 0
-#         high=n
-# 	    while low <= high:
-# 		    mid = (low + high ) // 2
-# 		    res = guess(mid)
-# 		    if res < 0:
-# 			    high = mid - 1
-
-# 		    elif res > 0:
-# 		        low = mid + 1
-
-# 		    else:
-# 			    return mid
