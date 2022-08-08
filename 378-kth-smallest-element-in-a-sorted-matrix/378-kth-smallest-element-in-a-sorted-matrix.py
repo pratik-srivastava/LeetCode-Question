963651f390e5bc2d4cc2d646249452b55917e602
Pratik Kumar Srivastava <86921160+pratik-srivastava@users.noreply.github.com>
2022-08-08 14:16:40 +0530

    Time: 267 ms (67.21%), Space: 18.7 MB (80.82%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.py
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.py
@@ -1,9 +1,7 @@
+from itertools import chain
 class Solution:
     def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
-        l=[]
-        for i in matrix:
-            for j in i:
-                l.append(j)
+        l=list(chain.from_iterable(matrix))
         l.sort()
         return l[k-1]
         
